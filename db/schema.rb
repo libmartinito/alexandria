4eed3300fbb8fd81a6de0093396f8668e3362808
lib.martinito@pm.me <lib.martinito@pm.me>
2023-07-23 21:27:26 +0800

    added the author model and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_23_131251) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_23_132105) do
+  create_table "authors", force: :cascade do |t|
+    t.string "given_name"
+    t.string "family_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "publishers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
